c6aca8a550233e6c8e19427a20d23529cd276724
winapiadmin <138602885+winapiadmin@users.noreply.github.com>
2026-06-24 14:09:30 +0700

    format

--- mio.hpp
+++ mio.hpp
@@ -1,3 +1,25 @@
+/*
+    An extraordinary fast PGN parser with mmap-based file loading for
+    large files and from existing buffer.
+
+    mio.hpp special notice: 
+    > Patched version of mio.hpp to define NOMINMAX for chesslib
+
+    Copyright (C) 2026  winapiadmin
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 /* Copyright 2017 https://github.com/mandreyel
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this
