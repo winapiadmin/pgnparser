f985dc258c69a0274ae839e6ba86fa2e9d46bc05
winapiadmin <138602885+winapiadmin@users.noreply.github.com>
2026-06-24 14:11:34 +0700

    format

--- mio.hpp
+++ mio.hpp
@@ -2,7 +2,7 @@
     An extraordinary fast PGN parser with mmap-based file loading for
     large files and from existing buffer.
 
-    mio.hpp special notice: 
+    mio.hpp special notice:
     > Patched version of mio.hpp to define NOMINMAX for chesslib
 
     Copyright (C) 2026  winapiadmin
